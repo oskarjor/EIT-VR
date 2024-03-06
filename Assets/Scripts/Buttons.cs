01d4b75cf8ab5c4f249e7add37a8742aaac17484
eirinsvi <eirin.svinsaas@gmal.com>
2024-03-06 15:32:32 +0100

    audio works

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -11,64 +11,62 @@ public class Buttons : MonoBehaviour
     public Transform table;
     public Transform boxPrefab;
 
-    public List<string> sentence1string = new List<string> () { "Jeg", "heter", "Nissanth"};
-    public AudioClip sentence1word1audio, sentence1word2audio, sentence1word3audio;
+    public List<string> sentence1string;
+    public AudioClip sentence1word1audio, sentence1word2audio, sentence1word3audio, sentence1word4audio, sentence1word5audio, sentence1word6audio;
+    
     public List<AudioClip> sentence1audioclips;
     public List<ListOfString> sentence1; 
     
     float moveX = 0;
 
     void Start(){
-        /**
+        
         sentence1audioclips= new List<AudioClip> () { 
             sentence1word1audio, 
             sentence1word2audio, 
-            sentence1word3audio
+            sentence1word3audio,
+            sentence1word4audio,
+            sentence1word5audio,
+            sentence1word6audio
         };
+
+        sentence1string = new List<string>() { "Hvordan", "går", "det", "med", "barnet", "mitt?" };
 
         sentence1 = new List<ListOfString> () { 
             new ListOfString(sentence1string[0], sentence1audioclips[0]),
             new ListOfString(sentence1string[1], sentence1audioclips[1]),
             new ListOfString(sentence1string[2], sentence1audioclips[2]),
+            new ListOfString(sentence1string[3], sentence1audioclips[3]),
+            new ListOfString(sentence1string[4], sentence1audioclips[4]),
+            new ListOfString(sentence1string[5], sentence1audioclips[5])
             };
 
         for (int i=0; i<sentence1.Count; i++){
             createButtonForWord(sentence1[i]);
         }
-        */
-
-        Debug.Log("This is a test");
-
-        createButtonForWord("hei");
-        createButtonForWord("hopp");
     }
 
     //Method for adding prefab with name and price and positioning under last scanned item
     //Adding price of item to total price
-    public void createButtonForWord(string listOfString)
+    public void createButtonForWord(ListOfString listOfString)
     {
         Debug.Log("i metode" + listOfString);
-        //string nameOfObject = scannedObject.name.Substring(0, scannedObject.name.IndexOf(" "));
-        //ItemStruct parentStruct = scannedObject.GetComponent<ItemStruct>();
-        //string name = parentStruct.name;
-
 
         Transform dynamicText = (Transform)Instantiate(buttonPrefab, new Vector3(buttonPrefab.position.x, buttonPrefab.position.y, buttonPrefab.position.z), Quaternion.Euler(90, 0, 0));
         dynamicText.transform.SetParent(table, false);
         dynamicText.transform.position += new Vector3(moveX, 0, 0);
-        dynamicText.gameObject.name = listOfString; //nameOfObject
+        dynamicText.gameObject.name = listOfString.word; //nameOfObject
+        dynamicText.gameObject.GetComponent<AudioSource>().clip = listOfString.wordPronunciation;
 
-        dynamicText.transform.Find("Canvas").Find("word").gameObject.GetComponent<TextMeshProUGUI>().text = listOfString;
+
+        dynamicText.transform.Find("Canvas").Find("word").gameObject.GetComponent<TextMeshProUGUI>().text = listOfString.word;
 
         Transform boxGameObject = (Transform)Instantiate(boxPrefab, new Vector3(boxPrefab.position.x, boxPrefab.position.y, boxPrefab.position.z), Quaternion.identity);
         boxGameObject.transform.SetParent(table, false);
         boxGameObject.transform.position += new Vector3(moveX, 0, 0);
-        boxGameObject.gameObject.name = listOfString; //nameOfObject
-
-
+        boxGameObject.gameObject.name = listOfString.word; //nameOfObject
 
         moveX += 1f;
-
     }
 }
 
