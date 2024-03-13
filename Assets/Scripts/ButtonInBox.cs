6041557d05d082c951a328d6de4b5282ee4da6ae
eirinsvi <eirin.svinsaas@gmal.com>
2024-03-13 11:28:52 +0100

    destroy gameobjects when task is done

--- Assets/Scripts/ButtonInBox.cs
+++ Assets/Scripts/ButtonInBox.cs
@@ -14,7 +14,10 @@ public class ButtonInBox : MonoBehaviour
             if (name == other.gameObject.name)
             {
                 other.gameObject.GetComponent<MeshRenderer>().material = greenMaterial;
-                buttonsScript.correctPositionedWords++;
+                buttonsScript.CheckIfTaskIsDone();
+                //tomake sure one object dont count as many correct words:
+                //set position of object nicely on top of the box
+                //disable grab interactable on the object
             }
             else other.gameObject.GetComponent<MeshRenderer>().material = redMaterial;
         }
--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -16,7 +16,7 @@ public class Buttons : MonoBehaviour
     
     public List<AudioClip> sentence1audioclips;
     public List<ListOfString> sentence1;
-
+    public List<GameObject> gameObjectsForSentence;
     public int correctPositionedWords = 0;
     
     float moveX = 0;
@@ -53,16 +53,12 @@ public class Buttons : MonoBehaviour
 
     public void createButtonForWord(ListOfString listOfString)
     {
-        Debug.Log("i metode" + listOfString);
-
         Transform dynamicText = (Transform)Instantiate(buttonPrefab, new Vector3(buttonPrefab.position.x, buttonPrefab.position.y, buttonPrefab.position.z), Quaternion.Euler(90, 0, 0));
         dynamicText.transform.SetParent(table, false);
         dynamicText.transform.position += new Vector3(moveX, 0, 0);
         dynamicText.gameObject.name = listOfString.word;
         dynamicText.gameObject.GetComponent<AudioSource>().clip = listOfString.wordPronunciation;
         dynamicText.gameObject.GetComponent<ButtonInBox>().buttonsScript = GetComponent<Buttons>();
-
-
         dynamicText.transform.Find("Canvas").Find("word").gameObject.GetComponent<TextMeshProUGUI>().text = listOfString.word;
 
         Transform boxGameObject = (Transform)Instantiate(boxPrefab, new Vector3(boxPrefab.position.x, boxPrefab.position.y, boxPrefab.position.z), Quaternion.identity);
@@ -70,8 +66,27 @@ public class Buttons : MonoBehaviour
         boxGameObject.transform.position += new Vector3(moveX, 0, 0);
         boxGameObject.gameObject.name = sentence1string[count];
 
+        gameObjectsForSentence.Add(boxGameObject.gameObject);
+        gameObjectsForSentence.Add(dynamicText.gameObject);
+
         moveX += 1f;
         count++;
+    }
+
+    public void CheckIfTaskIsDone()
+    {
+        correctPositionedWords++;
+        if(correctPositionedWords == sentence1string.Count)
+        {
+            StartCoroutine(destroyObjects());
+            //set task as complete in status script
+        }
+    }
+
+    IEnumerator destroyObjects()
+    {
+        yield return new WaitForSeconds(4);
+        foreach (GameObject go in gameObjectsForSentence) Destroy(go);
     }
 }
 
